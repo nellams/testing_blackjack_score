ad2517ce9c4d640ef3063d8138b0457634164d32
Sarah <nellamsem@gmail.com>
2017-04-02 19:40:30 -0700

    I didn't account for how to add a hand with more than one ace.

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,17 +1,37 @@
 require_relative 'deck'
 
 class Hand
+
   def initialize(cards)
     @cards = cards
   end
 
   def calculate_hand
-    # Your code here after writing tests
-    # Think about what this method needs to do
+    sum = 0
+    @cards.each do |card|
+      rank = card.split('')[0]
+      if card.split('').length > 2 || rank == "J" || rank == "Q" || rank == "K"
+        value = 10
+      elsif rank == "A"
+        value = 11
+        @boo = 1
+      else
+        value = rank.to_i
+      end
+      sum += value
+    end
+    if sum > 21
+      if @boo
+        sum -= 10
+      else
+      "fails"
+      end
+    else sum <= 21
+      sum
+    end
   end
 end
 
 deck = Deck.new
 cards = deck.deal(2)
 hand = Hand.new(cards)
-# hand.calculate_hand # Get this working properly
--- spec/lib/hand_spec.rb
+++ spec/lib/hand_spec.rb
@@ -4,22 +4,24 @@ describe Hand do
   # These UTF-8 characters will be useful for making different hands:
   # '♦', '♣', '♠', '♥'
 
-  let(:hand) { Hand.new(["10♦", "J♥"]) }
-  # You can add more sample hands
+  let(:hand) { Hand.new(["5♦", "3♥"]) }
+  let(:hand_2) { Hand.new(["K♥", "10♥", "2♠"])}
+  let(:hand_3) { Hand.new(["A♣", "9♣", "Q♣"])}
+  let(:hand_4) { Hand.new(["K♣", "10♣", "A♣"])}
+  let(:hand_5) { Hand.new(["2♣", "3♣", "A♣"])}
+
 
   describe "#calculate_hand" do
-    # We have included some example tests below. Change these once you get started!
-
     it "passes" do
-      # Use the RSpec keyword `expect`, as it appears below, to test your assertions
-      expect(1).to eq(1)
+      expect(hand.calculate_hand).to eq(8)
+      expect(hand_3.calculate_hand).to eq(20)
+      expect(hand_4.calculate_hand).to eq(21)
+      expect(hand_5.calculate_hand).to eq(16)
     end
 
     it "fails" do
-      expect(false).to eq true
+      expect(hand_2.calculate_hand).to eq("fails")
     end
-
-    # Add your remaining tests here.
 
   end
 end
